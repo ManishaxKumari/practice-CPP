7e104e167d0c565ceb4e29ae517bb978f8c479e3
Manisha <manisha857522@gmail.com>
2024-03-11 12:32:27 +0530

    ok

---
+++ justForLearn/practiceContest-CP/PracticeDSA/sorting/selection_sort.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void selection_sort(int arr[],int n){
+    for(int i=0;i<n;i++){
+        int mini=0;
+        for(int j=i;j<n-1;j++){
+            if(arr[j]<arr[mini]){
+                mini=j;
+            }
+        }
+        int temp= arr[mini];
+        arr[mini]=arr[i];
+        arr[i]=temp;
+
+    }
+}
+int main(){
+    //take array from user
+    int n;
+    cout<<"enter size of array"<<endl;
+    cin>>n;
+    cout<<"enter array element"<<endl;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    //sort 
+    selection_sort(arr,n);
+    //print array after sort
+    cout<<"your sorted array"<<endl;
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+
+    return 0;
+}

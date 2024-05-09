4f56ce3bf855e8b94db4cb8364e1b42de40f544c
Manisha <manisha857522@gmail.com>
2024-05-09 10:08:08 +0530

    ok

--- justForLearn/practiceContest-CP/PracticeDSA/sorting/selection_sort.cpp
+++ justForLearn/practiceContest-CP/PracticeDSA/sorting/selection_sort.cpp
@@ -2,8 +2,8 @@
 using namespace std;
 
 void selection_sort(int arr[],int n){
-    for(int i=0;i<n;i++){
-        int mini=0;
+    for(int i=0;i<n-2;i++){
+        int mini=i;
         for(int j=i;j<n-1;j++){
             if(arr[j]<arr[mini]){
                 mini=j;
--- justForLearn/practiceContest-CP/leetcode-day01.cpp
+++
@@ -1,5 +0,0 @@
-//watermelon-rating 800-4A
-
-#include<bits/stdc++.h>
-using namespace std;
-

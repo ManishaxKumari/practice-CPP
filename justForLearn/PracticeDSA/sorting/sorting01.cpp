824f248921c2624d27488ddc53fddd6b336dccab
Manisha <manisha857522@gmail.com>
2024-05-11 16:18:33 +0530

    ok

--- justForLearn/PracticeDSA/sorting/sorting01.cpp
+++ justForLearn/PracticeDSA/sorting/sorting01.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void selection_sort(int arr[],int n){
+void selection_sort01(int arr[],int n){
     for(int i=0;i<n-2;i++){
         int mini=i;
         for(int j=i;j<n-1;j++){
@@ -15,10 +15,27 @@ void selection_sort(int arr[],int n){
 
     }
 }
+
+void selectionSort02(int arr[], int n)
+    {
+       for(int i=0;i<n-1;i++){
+           int mini=i;
+           for(int j=i+1;j<n;j++){
+               if(arr[j]<arr[mini]){
+                   mini=j;
+               }
+           }
+               int temp=arr[mini];
+               arr[mini]=arr[i];
+               arr[i]=temp;
+           
+       }
+    }
+
 void bubble_sort(int arr[],int n){
     for(int i=n-1;i>=0;i--){
         int didswap=0;
-        for(int j=0;j<=i-1;j++){
+        for(int j=0;j<=i-1;j++){  //use i instead of (i-1) in gg for run code
             if (arr[j]>arr[j+1]){
                int temp= arr[j+1];
                 arr[j+1]=arr[j];
@@ -45,6 +62,8 @@ void insertion_sort(int arr[],int n){
 
    }
 }
+
+
 
 
 

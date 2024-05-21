61b22d54cd5869b4d592c2af29ae81bc8cff9a06
Manisha <manisha857522@gmail.com>
2024-05-21 17:41:25 +0530

    ok

--- justForLearn/PracticeDSA/binary_search.cpp/basic.cpp
+++ justForLearn/PracticeDSA/binary_search.cpp/basic.cpp
@@ -24,3 +24,23 @@ int bs(vector<int> &nums,int target){
 }
 
 //recursive code
+
+int bsr(vector<int> &nums,int low,int high,int target){
+    if(low>high){
+        return -1;
+    }
+    int mid=(low+high)/2;
+    if(nums[mid]==target){
+        return mid;
+    }
+    if(nums[mid]<target){
+        return bsr(nums,mid+1,high,target);
+    }
+    return bsr(nums,low,mid-1,target);
+}
+
+ int search(vector<int>& nums, int target) {
+        return(nums,0,nums.size()-1,target);
+    }
+
+    

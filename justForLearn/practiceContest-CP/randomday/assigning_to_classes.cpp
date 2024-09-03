d2a0b38868f4e1d5c50434e1506d74b3a9ce2882
Manisha <manisha857522@gmail.com>
2024-09-03 19:36:44 +0530

    ok

--- justForLearn/practiceContest-CP/randomday/assigning_to_classes.cpp
+++ justForLearn/practiceContest-CP/randomday/assigning_to_classes.cpp
@@ -9,16 +9,14 @@ int main()
     {
         int h;
         cin >> h;
-        vector<int> arr;
+        vector<int> arr(2 * h);
         for (int i = 0; i < 2 * h; i++)
         {
-            int s;
-            cin >> s;
-            arr.push_back(s);
+            cin >> arr[i];
         }
         sort(arr.begin(), arr.end());
-        int num1 = arr[h];
-        int num2 = arr[h + 1];
+        int num1 = arr[h - 1];
+        int num2 = arr[h];
         int AbDiff = abs(num1 - num2);
         cout << AbDiff;
     }
